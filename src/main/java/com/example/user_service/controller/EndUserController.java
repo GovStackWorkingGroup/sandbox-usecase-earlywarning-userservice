deba171d3f7a1d9dd12705ca41c9c2c724e9823f
vuk.damjanovic <vuk.damjanovic@3stepit.com>
2024-11-01 15:34:19 +0200

    Update end-user endpoint; Temp docker network config

--- src/main/java/com/example/user_service/controller/EndUserController.java
+++ src/main/java/com/example/user_service/controller/EndUserController.java
@@ -3,6 +3,7 @@ package com.example.user_service.controller;
 import com.example.user_service.models.dtos.EndUserDto;
 import com.example.user_service.models.dtos.EndUserRequestDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,7 +13,7 @@ public interface EndUserController {
 
     @GetMapping(path = "/getEndUsersForCountry")
     List<EndUserDto> getEndUsersForCountry(@RequestParam int countryId);
-    @GetMapping(path = "/getEndUsersForCounty")
+    @PostMapping(path = "/getEndUsersForCounty")
     List<EndUserDto> getEndUsersForCounty(@RequestBody EndUserRequestDto requestDto);
 
 }
